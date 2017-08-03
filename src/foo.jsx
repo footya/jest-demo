c55bfe61699fff663a303d642a8f6372afdf2c5f
footya <footya@gmail.com>
2017-08-03 17:42:54 +0800

    add input change test

--- src/foo.jsx
+++ src/foo.jsx
@@ -6,12 +6,29 @@ const defaultProps = {};
 export default class Foo extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+    	isChecked: false,
+    };
+    this.onChange = this.onChange.bind(this);
   }
 
+  onChange() {
+  	this.setState({
+  		isChecked: !this.state.isChecked
+  	});
+  }
   render() {
     return (
       <div className="foo" >
       	<h2>title{this.props.name}</h2>
+      	<label>
+	        <input
+	          type="checkbox"
+	          checked={this.state.isChecked}
+	          onChange={this.onChange}
+	        />
+	        {this.state.isChecked ? 'on' : 'off'}
+	    </label>
       </div>
     );
   }
--- src/foo.test.js
+++ src/foo.test.js
@@ -6,11 +6,10 @@ import {
 } from 'enzyme';
 
 import Foo from './foo.jsx';
-jest.dontMock('./foo.jsx');
+// jest.dontMock('./foo.jsx');
 
 describe("Foo component", function() {
   const fooCom = shallow(<Foo name="good"/>);
-
   it("no name test", () => {
     expect(shallow(<Foo />).find('h2').text()).toEqual('titlefootya');
   });
@@ -21,5 +20,20 @@ describe("Foo component", function() {
 
   it("class name is .foo", () => {
     expect(fooCom.find('.foo').length).toBe(1);
+  });
+
+  // 点击测试
+  it("checkboxLabel click test", () => {
+    // const checkboxLabel = fooCom.find('label');
+    expect(fooCom.find('label').text()).toEqual('off');
+    fooCom.find('input').simulate('change');
+    // 注意此处不能用变量checkboxLabel获取text，貌似这个变量获取到的只是赋值当时情况下的common节点
+    // expect(checkboxLabel.text()).toEqual('on');
+    expect(fooCom.find('label').text()).toEqual('on');
+    fooCom.find('input').simulate('change');
+    expect(fooCom.find('label').text()).toEqual('off');
+    fooCom.find('input').simulate('change');
+    expect(fooCom.find('label').text()).toEqual('on');
+
   });
 });
